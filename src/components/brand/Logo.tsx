f58c2c0c5ced13a9efe387c248e028fca04b7f23
samvidh75 <samvidhmehta@gmail.com>
2026-06-24 22:18:37 +0530

    chore(tokens): add public research design system

---
+++ src/components/brand/Logo.tsx
@@ -0,0 +1,2 @@
+import logoMark from "../../assets/logo-mark.svg";
+export default function Logo(){return <div className="brand"><img src={logoMark} width="32" height="32" alt="StockStory"/><div><b>StockStory</b><small>India</small></div></div>}
